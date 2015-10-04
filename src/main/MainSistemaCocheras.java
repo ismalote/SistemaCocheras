ced916ccd59b63b7aad14cbf2f225462fa10fcbe
skynet-pc <skynet-pc@Skynet>
2015-10-03 21:30:38 -0300

    Cuando dabas alta de contrato decia "modificar contrato", ya lo corregi,

--- src/main/MainSistemaCocheras.java
+++ src/main/MainSistemaCocheras.java
@@ -945,7 +945,7 @@ public class MainSistemaCocheras {
 		try {
 			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 			
-			System.out.println("Crear Contrato");
+			System.out.println("ALTA DE CONTRATO");
 			System.out.println("------------------------");
 			System.out.print("DNI: ");
 			String dni = reader.readLine();
